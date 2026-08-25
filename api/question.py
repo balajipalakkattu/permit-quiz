60dbdbf93bb79f5ecdb397135b4200f6b5fae4a7
Balaji Gopalakrishnan <balajipalakkattu@gmail.com>
2026-08-24 20:32:05 -0500

    Update QUESTIONS_FILE path to be dynamic

--- api/question.py
+++ api/question.py
@@ -3,7 +3,8 @@ import json, os
 
 app = Flask(__name__)
 
-QUESTIONS_FILE = "questions.json"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+QUESTIONS_FILE = os.path.join(BASE_DIR, "questions.json")
 
 def load_questions():
     with open(QUESTIONS_FILE, "r") as f:
